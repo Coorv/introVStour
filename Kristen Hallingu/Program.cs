f6ca1e136225c8c7ece4dab9f3a593fa10fdcd1a
Opilane TTHK <opilane@tthk.ee>
2025-09-19 14:33:53 +0300

    Ülesanne 2 joonis

--- Kristen Hallingu/Program.cs
+++ Kristen Hallingu/Program.cs
@@ -1,36 +1,152 @@
-﻿int arv1 = 0;
-int arv2 = 0;
-Console.WriteLine("Tere, palun sisesta esimene arv: ");
-arv1 = int.Parse(Console.ReadLine());
-Console.WriteLine("palun sisesta teine arv: ");
-arv2 = int.Parse(Console.ReadLine());
-Console.WriteLine("Missugust tehet soovid teha? Kirjuta kas: + - / * ^^");
-string tehtetüüp = Console.ReadLine();
-int tehe = 0;
+﻿//int arv1 = 0;
+//int arv2 = 0;
+//Console.WriteLine("Tere, palun sisesta esimene arv: ");
+//arv1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("palun sisesta teine arv: ");
+//arv2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Missugust tehet soovid teha? Kirjuta kas: + - / * ^^");
+//string tehtetüüp = Console.ReadLine();
+//int tehe = 0;
 
-if (tehtetüüp == "+"  /*tingimus siia*/)
-{ 
-    tehe = arv1 + arv2;
-}
+//if (tehtetüüp == "+"  /*tingimus siia*/)
+//{ 
+//    tehe = arv1 + arv2;
+//}
 
-if (tehtetüüp == "-")
+//if (tehtetüüp == "-")
+//{
+//    tehe = arv1 - arv2;
+//}
+
+//if (tehtetüüp == "*")
+//{
+//    tehe = arv1 * arv2;
+//}
+
+//if (tehtetüüp == "/")
+//{
+//    tehe = arv1 / arv2;
+//}
+
+//if (tehtetüüp == "^^")
+//{
+//    tehe = (int)Math.Pow(arv1, arv2);
+//}
+
+//Console.WriteLine($"{tehtetüüp} Tehte tulemus: {tehe}");
+string lemmikpuuvili = "";
+string valik = "";
+string lemmikjuurvili;
+Console.WriteLine("Kas sulle meeldivad puuviljad või juurviljad, puulviljad kui puuvili, juurviljad kui juurvili");
+valik = Console.ReadLine().ToString();
+
+
+if (valik == "puuviljad")
 {
-    tehe = arv1 - arv2;
+    Console.WriteLine("Milline puuvili sulle lemmik?");
+    lemmikpuuvili = Console.ReadLine();
+    switch (lemmikpuuvili)
+    {
+        case "maasikas":
+            Console.WriteLine("Maasikas ei ole puuvili");
+            break;
+        case "pirn":
+            Console.WriteLine("Kas hapu pirn või nagus pirn, mõlemad on head.");
+            break;
+        case "kartul":
+            Console.WriteLine("See ei kasva isegi puu otsas");
+            break;
+        case "kahvel":
+            Console.WriteLine("Kahvel ei ole söödav ega puuvili");
+            break;
+        case "taldrik":
+            Console.WriteLine("Taldrik on see mille peal sa asju sööd mitte vastupidi");
+            break;
+        default:
+            Console.WriteLine("Ei tunne seda puuvilja");
+            break;
+    }
+
+    //if (lemmikpuuvili == "maasikas")
+    //{
+    //    Console.WriteLine("Maasikas ei ole puuvili");
+    //}
+    //else if (lemmikpuuvili == "pirn")
+    //{
+        
+    //}
+    //else if (lemmikpuuvili == "kartul")
+    //{
+    //    
+    //}
+    //else if (lemmikpuuvili == "kahvel")
+    //{
+    //    Console.WriteLine("Kahvel ei ole söödav ega puuvili");
+    //}
+    //else if (lemmikpuuvili == "taldrik")
+    //{
+    //    Console.WriteLine("Taldrik on see mille peal sa asju sööd mitte vastupidi");
+    //}
+    //else
+    //{
+    //    Console.WriteLine("Ei tunne seda puuvilja");
+    //}
+
 }
 
-if (tehtetüüp == "*")
+else if (valik == "juurviljad")
 {
-    tehe = arv1 * arv2;
-}
+    Console.WriteLine("Milline juurvili sulle lemmik?");
+    lemmikjuurvili = Console.ReadLine();
 
-if (tehtetüüp == "/")
+    switch (lemmikjuurvili)
+    {
+        case "porgand":
+            Console.WriteLine("Porgand on hea juurvili");
+            break;
+        case "õun":
+            Console.WriteLine("See ei ole üldse juurvili.");
+            break;
+        case "apelsiin":
+            Console.WriteLine("See kasvab puuotsas");
+            break;
+        case "kahvel":
+            Console.WriteLine("Kahvel ei ole söödav ega juurvili");
+            break;
+        case "taldrik":
+            Console.WriteLine("Taldrik on see mille peal sa asju sööd mitte vastupidi");
+            break;
+        default:
+            Console.WriteLine("Ei tunne seda juurvilja");
+            break;
+    }
+    //if (lemmikjuurvili == "porgand")
+    //{
+    //    Console.WriteLine("Porgand on hea juurvili");
+    //}
+    //else if (lemmikjuurvili == "õun")
+    //{
+    //    Console.WriteLine("See ei ole üldse juurvili.");
+    //}
+    //else if (lemmikjuurvili == "apelsiin")
+    //{
+    //    Console.WriteLine("See kasvab puuotsas");
+    //}
+    //else if (lemmikjuurvili == "kahvel")
+    //{
+    //    Console.WriteLine("Kahvel ei ole söödav ega juurvili");
+    //}
+    //else if (lemmikjuurvili == "taldrik")
+    //{
+    //    Console.WriteLine("Taldrik on see mille peal sa asju sööd mitte vastupidi");
+    //}
+    //else
+    //{
+    //    Console.WriteLine("Ei tunne seda juurvilja");
+    //}
+
+}
+else
 {
-    tehe = arv1 / arv2;
+    Console.WriteLine("Seda valikut ei ole");
 }
-
-if (tehtetüüp == "^^")
-{
-    tehe = (int)Math.Pow(arv1, arv2);
-}
-
-Console.WriteLine($"{tehtetüüp} Tehte tulemus: {tehe}");
